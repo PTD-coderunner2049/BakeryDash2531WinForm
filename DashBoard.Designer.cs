7ea49f185c8ed475dd6c912c3bbd8d9caa013cc1
iloveMlord <ilovemilord.op@gmail.com>
2025-11-14 04:01:53 +0700

    Trashest Login frame in, just so there are something

--- DashBoard.Designer.cs
+++ DashBoard.Designer.cs
@@ -28,19 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.dashMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xXToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.xXToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextUserName = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripTextGreet = new System.Windows.Forms.ToolStripTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dashMenuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(570, 261);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(97, 19);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "DASHBOARD";
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.materialDivider1.Location = new System.Drawing.Point(0, 90);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(1264, 10);
+            this.materialDivider1.TabIndex = 1;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // dashMenuStrip
+            // 
+            this.dashMenuStrip.BackColor = System.Drawing.SystemColors.Control;
+            this.dashMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.xXToolStripMenuItem,
+            this.xXToolStripMenuItem1,
+            this.xXToolStripMenuItem2,
+            this.toolStripTextGreet,
+            this.toolStripTextUserName});
+            this.dashMenuStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.dashMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.dashMenuStrip.Name = "dashMenuStrip";
+            this.dashMenuStrip.Size = new System.Drawing.Size(1264, 24);
+            this.dashMenuStrip.TabIndex = 2;
+            this.dashMenuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // xXToolStripMenuItem
+            // 
+            this.xXToolStripMenuItem.Name = "xXToolStripMenuItem";
+            this.xXToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.xXToolStripMenuItem.Text = ">(V.V)<";
+            // 
+            // xXToolStripMenuItem1
+            // 
+            this.xXToolStripMenuItem1.Name = "xXToolStripMenuItem1";
+            this.xXToolStripMenuItem1.Size = new System.Drawing.Size(60, 20);
+            this.xXToolStripMenuItem1.Text = ">(V.V)<";
+            // 
+            // xXToolStripMenuItem2
+            // 
+            this.xXToolStripMenuItem2.Name = "xXToolStripMenuItem2";
+            this.xXToolStripMenuItem2.Size = new System.Drawing.Size(60, 20);
+            this.xXToolStripMenuItem2.Text = ">(V.V)<";
+            // 
+            // toolStripTextUserName
+            // 
+            this.toolStripTextUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.toolStripTextUserName.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.toolStripTextUserName.Enabled = false;
+            this.toolStripTextUserName.MaxLength = 50767;
+            this.toolStripTextUserName.Name = "toolStripTextUserName";
+            this.toolStripTextUserName.ReadOnly = true;
+            this.toolStripTextUserName.Size = new System.Drawing.Size(500, 20);
+            this.toolStripTextUserName.Text = "user ....";
+            // 
+            // toolStripTextGreet
+            // 
+            this.toolStripTextGreet.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.toolStripTextGreet.Enabled = false;
+            this.toolStripTextGreet.Name = "toolStripTextGreet";
+            this.toolStripTextGreet.Size = new System.Drawing.Size(55, 20);
+            this.toolStripTextGreet.Text = "Greeting!!";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.materialDivider1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 100);
+            this.panel1.TabIndex = 5;
             // 
             // DashBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.dashMenuStrip);
             this.Name = "DashBoard";
             this.Text = "DashBoard";
+            this.dashMenuStrip.ResumeLayout(false);
+            this.dashMenuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private System.Windows.Forms.MenuStrip dashMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xXToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem xXToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextUserName;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextGreet;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- LogIn.Designer.cs
+++ LogIn.Designer.cs
@@ -28,21 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4.SuspendLayout();
+            this.panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Font = new System.Drawing.Font("Sans Serif Collection", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialFlatButton1.Location = new System.Drawing.Point(99, 158);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(52, 36);
+            this.materialFlatButton1.TabIndex = 9;
+            this.materialFlatButton1.Text = "LOGIN";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Enabled = false;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(180, 377);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(161, 19);
+            this.materialLabel2.TabIndex = 10;
+            this.materialLabel2.Text = "Never had an account?";
+            this.materialLabel2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel9);
+            this.panel4.Controls.Add(this.materialFlatButton1);
+            this.panel4.Location = new System.Drawing.Point(85, 107);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(352, 135);
+            this.panel4.TabIndex = 12;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel9.Controls.Add(this.materialSingleLineTextField1);
+            this.panel9.Controls.Add(this.pictureBox4);
+            this.panel9.Location = new System.Drawing.Point(0, 62);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(352, 50);
+            this.panel9.TabIndex = 19;
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Font = new System.Drawing.Font("Sans Serif Collection", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(56, 13);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(284, 23);
+            this.materialSingleLineTextField1.TabIndex = 1;
+            this.materialSingleLineTextField1.Text = "...";
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::BakeryDash2531.Properties.Resources.B;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(80, 503);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::BakeryDash2531.Properties.Resources.A;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(444, -2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(80, 503);
+            this.panel2.TabIndex = 14;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(85, 399);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(352, 65);
+            this.materialDivider1.TabIndex = 15;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialFlatButton2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.materialFlatButton2.Location = new System.Drawing.Point(0, 0);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(352, 53);
+            this.materialFlatButton2.TabIndex = 16;
+            this.materialFlatButton2.Text = "LOGIN";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.materialFlatButton2);
+            this.panel5.Location = new System.Drawing.Point(85, 253);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(352, 53);
+            this.panel5.TabIndex = 17;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.materialFlatButton3);
+            this.panel8.Location = new System.Drawing.Point(85, 405);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(352, 53);
+            this.panel8.TabIndex = 18;
+            // 
+            // materialFlatButton3
+            // 
+            this.materialFlatButton3.AutoSize = true;
+            this.materialFlatButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton3.Depth = 0;
+            this.materialFlatButton3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialFlatButton3.Location = new System.Drawing.Point(0, 0);
+            this.materialFlatButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton3.Name = "materialFlatButton3";
+            this.materialFlatButton3.Primary = false;
+            this.materialFlatButton3.Size = new System.Drawing.Size(352, 53);
+            this.materialFlatButton3.TabIndex = 16;
+            this.materialFlatButton3.Text = "SIGN UP";
+            this.materialFlatButton3.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.Controls.Add(this.materialSingleLineTextField2);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(85, 107);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(352, 50);
+            this.panel3.TabIndex = 20;
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Font = new System.Drawing.Font("Sans Serif Collection", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(56, 13);
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '\0';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(284, 23);
+            this.materialSingleLineTextField2.TabIndex = 1;
+            this.materialSingleLineTextField2.Text = "...";
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "BAKERY DASH";
             // 
             // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(524, 501);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.materialLabel2);
             this.Name = "LogIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Loging in...";
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel8;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel3;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
