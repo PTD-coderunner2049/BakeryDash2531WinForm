f472d281638e28adf7132c62e7912da4af8af859
iloveMlord <ilovemilord.op@gmail.com>
2025-11-14 02:25:46 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using BakeryDash.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BakeryDash2531
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LogIn());
+
+            Class1 a = new Class1();
+            a.a();
+        }
+    }
+}
